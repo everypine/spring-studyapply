24b21ca16c64abb54c50526ee69a561c5664c3d7
everypine <kenton.lim@gmail.com>
2026-06-23 11:10:23 +0900

    initial commit

---
+++ src/main/java/studyapply/StudyApplyApplication.java
@@ -0,0 +1,12 @@
+package studyapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplyApplication.class, args);
+    }
+}
---
+++ src/test/java/studyapply/StudyApplyApplicationTests.java
@@ -0,0 +1,12 @@
+package studyapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
